132eaec88a1f58191ef9a0dfe65e72c698b9bbba
batoan1221 <batoan1221@gmail.com>
2013-11-30 10:26:48 +0700

    Initial Commit

---
+++ JKShop/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JKShop
+//
+//  Created by admin on 11/30/13.
+//  Copyright (c) 2013 Nguyễn Bá Toàn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JKAppDelegate class]));
+    }
+}
