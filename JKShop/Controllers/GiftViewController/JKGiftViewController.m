06434a66ce81a887e1ccb291aa3713fa64e5e249
Toan Slan <batoan1221@gmail.com>
2013-12-19 16:56:52 +0700

    Building feature shake to get lucky gift

--- JKShop/Controllers/GiftViewController/JKGiftViewController.m
+++ JKShop/Controllers/GiftViewController/JKGiftViewController.m
@@ -9,6 +9,7 @@
 #import "JKGiftViewController.h"
 
 static int const NOTIF_VIEW_HEIGHT = 40;
+static NSString * const kGiftUserDefault = @"kGiftUserDefault";
 
 @interface JKGiftViewController ()
 <
@@ -17,16 +18,27 @@ CLLocationManagerDelegate
 
 @property (weak, nonatomic) IBOutlet UIView *shakeNotificationView;
 @property (strong, nonatomic) CLLocationManager *locationManager;
+@property (strong, nonatomic) NSArray *giftArray;
 
 @end
 
 @implementation JKGiftViewController
 
+- (NSArray *)giftArray{
+    if (self.giftArray == nil) {
+        self.giftArray = [[NSArray alloc] init];
+        self.giftArray = @[@"You got discount 10% on T-Shirt",
+                           @"You got discount 10% on Belt",
+                           @"You got discount 10% on Jean",
+                           @"You got discount 10% on Shoes"];
+    }
+    return self.giftArray;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     self.title = @"Lucky Gift";
-    [self showNotificationView];
     [self setUpLocationManager];
 }
 
@@ -34,20 +46,31 @@ CLLocationManagerDelegate
     self.locationManager = [[CLLocationManager alloc] init];
     self.locationManager.delegate = self;
     self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-    
     [self.locationManager startUpdatingLocation];
 }
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
 {
-    NSLog(@"didUpdateToLocation: %@", newLocation);
-    CLLocation *currentLocation = newLocation;
+//    CLLocation *currentLocation = newLocation;
+    
+    CLLocation *currentLocation = [[CLLocation alloc] initWithLatitude:10.778445 longitude:106.666297];
     
     if (currentLocation != nil) {
         CLLocation *shopLocation = [[CLLocation alloc] initWithLatitude:SETTINGS_JK_SHOP_LATITUDE longitude:SETTINGS_JK_SHOP_LONGITUDE];
         CLLocationDistance dist = [currentLocation distanceFromLocation:shopLocation];
-        int kilometers = dist/1000;
-        DLog(@"%i",kilometers);
+        CGFloat kilometers = dist/1000.0;
+        [self.locationManager stopUpdatingLocation];
+        
+        if (kilometers <= 0.5f && ![[NSUserDefaults standardUserDefaults] valueForKey:kGiftUserDefault]) {
+            [self showNotificationView];
+            return;
+        }
+        if (kilometers > 0.5f) {
+            [SVProgressHUD showErrorWithStatus:@"You are so far from JK Shop!"];
+            return;
+        }
+        
+        [SVProgressHUD showErrorWithStatus:@"You have a gift for today already!"];
     }
 }
 
@@ -85,8 +108,27 @@ CLLocationManagerDelegate
 {
     if (motion == UIEventSubtypeMotionShake) {
         [self hideNotificationView];
+        
+        NSInteger rand = [self randomIntegerFrom:0 to:3];
+        [self showAlertViewWithContent:self.giftArray[rand]];
+        
+        [[NSUserDefaults standardUserDefaults] setValue:self.giftArray[rand] forKey:kGiftUserDefault];
     }
     
+}
+
+- (void)showAlertViewWithContent:(NSString *)content
+{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Congratulation!"
+                                                    message:[NSString stringWithFormat:@"%@ for today",content]
+                                                   delegate:self
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
+}
+
+- (NSInteger)randomIntegerFrom:(NSInteger)min to:(NSInteger)max{
+    return ((arc4random() % (max-min+1)) + min);
 }
 
 @end
