25b0c4bd16279280e2a757ac2301691d2b2857e3
Ba Toan <batoan1221@gmail.com>
2014-07-30 17:02:55 +0700

    Added base appdelegate service

---
+++ JKShop/Misc/BaseAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  BaseAppDelegate.h
+//  JKShop
+//
+//  Created by iSlan on 7/30/14.
+//  Copyright (c) 2014 Nguyễn Bá Toàn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseAppDelegate : UIResponder <UIApplicationDelegate>
+
+@end
---
+++ JKShop/Misc/BaseAppDelegate.m
@@ -0,0 +1,80 @@
+//
+//  BaseAppDelegate.m
+//  JKShop
+//
+//  Created by iSlan on 7/30/14.
+//  Copyright (c) 2014 Nguyễn Bá Toàn. All rights reserved.
+//
+
+#import "BaseAppDelegate.h"
+
+@interface BaseAppDelegate()
+
+@property (strong, nonatomic) NSArray *services;
+
+@end
+
+@implementation BaseAppDelegate
+
+- (NSArray *)services {
+    if (!_services) {
+        _services = @[];
+    }
+    
+    return _services;
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    id <UIApplicationDelegate> service;
+    for (service in self.services) {
+        if ([service respondsToSelector:@selector(application:didFinishLaunchingWithOptions:)]) {
+            [service application:application didFinishLaunchingWithOptions:launchOptions];
+        }
+    }
+    
+    return YES;
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    id <UIApplicationDelegate> service;
+    for (service in self.services) {
+        if ([service respondsToSelector:@selector(applicationDidBecomeActive:)]) {
+            [service applicationDidBecomeActive:application];
+        }
+    }
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    id <UIApplicationDelegate> service;
+    for (service in self.services) {
+        if ([service respondsToSelector:@selector(applicationWillTerminate:)]) {
+            [service applicationWillTerminate:application];
+        }
+    }
+}
+
+ - (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    id <UIApplicationDelegate> service;
+    for (service in self.services) {
+        if ([service respondsToSelector:@selector(applicationDidEnterBackground:)]) {
+            [service applicationDidEnterBackground:application];
+        }
+    }
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    id <UIApplicationDelegate> service;
+    for (service in self.services) {
+        if ([service respondsToSelector:@selector(application:openURL:sourceApplication:annotation:)]) {
+            [service application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
+        }
+    }
+    return YES;
+}
+
+@end
