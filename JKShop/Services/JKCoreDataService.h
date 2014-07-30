15e653fcadc558eca6cfd7069c5bb8812f2aa256
Ba Toan <batoan1221@gmail.com>
2014-07-30 17:00:49 +0700

    Added core data service

---
+++ JKShop/Services/JKCoreDataService.h
@@ -0,0 +1,17 @@
+//
+//  JKCoreDataService.h
+//  JKShop
+//
+//  Created by iSlan on 7/30/14.
+//  Copyright (c) 2014 Nguyễn Bá Toàn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JKCoreDataService : NSObject <UIApplicationDelegate>
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@end
